c0d78772862598eda089d1625b94a04be97ef68d
AutomationUser <AutomationUser@cognizant.com>
2021-07-21 12:16:26 +0000

    Adding file

--- scripts/wr_automation.py
+++ scripts/wr_automation.py
@@ -44,7 +44,7 @@ resource_path = '/home/ubuntu/mugunthan/repo/MFTAuto/scripts/resources'
 req_fields_file = resource_path + '/requiredfields.properties'
 
 #Template files config path
-aact_tempalte_file = resource_path + '/accttemplate.json'
+acct_tempalte_file = resource_path + '/accttemplate.json'
 usr_template_file = resource_path + '/usertemplate.json'
 ssh_template_file = resource_path + '/sshtemplate.json'
 ftp_template_file = resource_path + '/ftptemplate.json'
@@ -131,7 +131,113 @@ with open( wr_csv_file ) as csvfile:
                 logging.info( 'Service Account Subscription dir exists: '+ sub_dir )
 
         if create_update == MOD_CREATE:
-            if module 
+            if module == ACCT_MOD:
+                with open ( acct_tempalte_file ) as acct_template_data:
+                    new_data = json.load( acct_template_data )
+                    for k,v in dict.items():
+                        if k in new_data:
+                            new_data[k] = v
+                        elif k == 'email':
+                            new_data['contact']['email'] = v
+                        elif k == 'phone':
+                            new_data['contact']['phone'] = v
+                        else:
+                            logging.info( 'Key/Not Found in the tempalte: ' + k )
+            elif module == USR_MOD:
+                with open ( user_tempalte_file ) as user_template_data:
+                    new_data = json.load( user_template_data )
+                    for k,v in dict.items():
+                        if k in new_data:
+                            new_data[k] = v
+                        elif k == 'username':
+                            new_data['users'][0]['passwordCredentials']['username'] = v
+                        elif k == 'password':
+                            new_data['users'][0]['passwordCredentials']['password'] = v
+                        elif k in new_data['users'][0]:
+                            new_data['users'][0][k] = v
+                        else:
+                            logging.info( 'Key/Not Found in the tempalte: ' + k )
+            elif module == TS_MOD:
+                if 'account' not in dict:
+                    dict['acctount'] = acct
+                if 'name' not in dict:
+                    dict['name'] = componentname 
+                if ts_protocol == SSH_PROTOCOL:
+                    with open ( ssh_template_file ) as ssh_tempalte_data:
+                        new_data = json.load( ssh_tempalte_data )
+                        for k,v in dict.items():
+                            new_data[k] = v
+                elif ts_protocol == FTP_PROTOCOL:
+                    with open ( ftp_template_file ) as ftp_tempalte_data:
+                        new_data = json.load( ftp_tempalte_data )
+                        for k,v in dict.items():
+                            new_data[k] = v
+                elif ts_protocol == SMB_PROTOCOL:
+                    with open ( smb_template_file ) as smb_tempalte_data:
+                        new_data = json.load( smb_tempalte_data )
+                        for k,v in dict.items():
+                            new_data[k] = v
+                elif ts_protocol == FM_PROTOCOL:
+                    with open ( fm_template_file ) as fm_tempalte_data:
+                        new_data = json.load( fm_tempalte_data )
+                        for k,v in dict.items():
+                            new_data[k] = v
+             elif module == SUB_MOD:
+                 with open ( sub_template_file ) as sub_tempalte_data:
+                     new_data = json.load( sub_tempalte_data)
+                     for k,v in dict.items():
+                         new_data[k] = v
+        if new_data:
+            with open ( json_filepath, 'w' ) as write_to_file:
+                json.dump( new_data, write_to_file, indent = 4 )
+                logging.info( 'New file successfully created: '+ json_filepath )
+
+        if create_update == MOD_UPDATE:
+            if os.path.exists ( json_filepath ):
+                with open ( json_filepath, 'r' ) as open_json_file:
+                    file_data = json.load( open_json_file )
+                    if module == ACCT_MOD:
+                        for k,v in dict.items():
+                            if k in file_data:
+                                file_data[k] = v
+                            elif k == 'email':
+                                file_data['contact']['email'] = v
+                            elif k == 'phone':
+                                file_data['contact']['phone'] = v
+                            else:
+                                logging.info( 'Key/Not Found in the tempalte: ' + k )
+                    elif module == USR_MOD:
+                        for k,v in dict.items():
+                            if k in file_data:
+                                file_data[k] = v
+                            elif k == 'username':
+                                file_data['users'][0]['passwordCredentials']['username'] = v
+                            elif k == 'password':
+                                file_data['users'][0]['passwordCredentials']['password'] = v
+                            elif k in file_data['users'][0]:
+                                file_data['users'][0][k] = v
+                            else:
+                                logging.info( 'Key/Not Found in the tempalte: ' + k )
+                    elif module == TS_MOD or module == SUB_MOD:
+                        for k,v in dict.items():
+                            if k in file_data:
+                                logging.info( 'Old value for '+ k +': '+ file_data[k])
+                                file_data[k] = v
+                                logging.info( 'New value for '+ k +': '+ file_data[k])
+                            else:
+                                logging.info( 'Inserting new key & value '+ k +': '+ file_data[k])
+                                file_data[k] = v
+                with open ( json_filepath, 'w' ) as write_json_file:
+                    json.dump( file_data, write_json_file, indent = 4 )
+        logging.info( 'Verification and validation started for new json file' )
+        validation_status = required_fields_check( log_filename, required_fields_file, module, acct, componentname, json_filepath, json_filepath )
+        logging.info( 'Verification and validation status '+ validation_status + 'for file '+ json_filepath)
+        if validation_status != STATUS_PASS:
+            logging.info( 'Please check the reason for failure and fix it and re-run' )
+
+
+                            
+            
 
 
 
